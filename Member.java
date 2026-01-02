3f84fb1c3b9565b10ed6af12079d746f63baf0cb
Tejaswini-A06 <2311cs010362@mallareddyuniversity.ac.in>
2026-01-02 11:30:31 +0530

    Add files via upload

---
+++ Member.java
@@ -0,0 +1,17 @@
+public class Member {
+    String name;
+    int age;
+
+    void displayInfo()
+    {
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+    public static void main(String[] args){
+        Member obj=new Member();
+        obj.name="Tejaswini";
+        obj.age=20;
+
+        obj.displayInfo();
+    }
+}
---
+++ varArgs.java
@@ -0,0 +1,15 @@
+public class varArgs {
+    static int sum(int... numbers)
+    {
+        int total=0;
+        for(int n:numbers)
+        {
+            total=total+n;
+        }
+        return total;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(sum(1, 2));
+    }
+}
